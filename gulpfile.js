b91f8d604b0e2cbe732a472f546e9ac510990e93
fflorezz <felipeflorezart@gmail.com>
2020-02-11 16:36:43 -0500

    Config Browser-sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,25 @@
 const sass = require("gulp-sass");
 const gulp = require("gulp");
+const browserSync = require("browser-sync").create();
 sass.compiler = require("node-sass");
 
 function style() {
-	return gulp.src("./sass/**/*.scss").pipe(sass().on("error", sass.logError)).pipe(gulp.dest("./css"));
+	return gulp
+		.src("./sass/**/*.scss")
+		.pipe(sass().on("error", sass.logError))
+		.pipe(gulp.dest("./css"))
+		.pipe(browserSync.stream());
 }
 
 function watch() {
+	browserSync.init({
+		server: {
+			baseDir: "./"
+		}
+	});
 	gulp.watch("./sass/**/*.scss", style);
+	gulp.watch("./*.html").on("change", browserSync.reload);
+	gulp.watch("./js/**/*.js").on("change", browserSync.reload);
 }
 
 exports.style = style;
